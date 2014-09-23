374cf56f96bdd8c221b4dcbaf7359825e716e62d
Robert Figueras <rfiguera@dhcp-205.chicago.bravo>
2014-09-23 16:03:24 -0500

    Refactor and remove NRDSwitchCalculator class, replace with collection operator
    Creating a helper object to calculate simple addition and manage a counter property is a bit of overhead. The use of the simple collection operator @sum on the "on" property of the UISwitches in the IBOutletCollection replaces the purpose of the NRDSwitchCalculator. This provides a significant reduction in code.

--- FlippingSwitches/NRDViewController.m
+++ FlippingSwitches/NRDViewController.m
@@ -7,13 +7,11 @@
 //
 
 #import "NRDViewController.h"
-#import "NRDSwitchCalculator.h"
 #import "UISwitch+NRDForcedAnimation.h"
 
 @interface NRDViewController ()
 @property (nonatomic, strong) IBOutletCollection(UISwitch) NSArray *childSwitches;
 @property (nonatomic, weak) IBOutlet UISwitch *masterSwitch;
-@property (nonatomic, strong) NRDSwitchCalculator *calculator;
 @end
 
 @implementation NRDViewController
@@ -21,50 +19,32 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.calculator = [[NRDSwitchCalculator alloc] init];
-    self.calculator.totalNumberOfSwitches = self.childSwitches.count;
 }
 
 
 #pragma mark Action Methods
 
-- (IBAction)flippedSwitch:(UISwitch *)childSwitch
+- (IBAction)flippedChildSwitch
 {
-    NSInteger childSwitchValue = childSwitch.on;
+    // use sum collection operator to provide calculated results
+    NSInteger numberOfSwitchesTurnedOn = [[self.childSwitches valueForKeyPath:@"@sum.on"] intValue];
+    NSInteger totalNumberOfChildSwitches = self.childSwitches.count;
     
-    // calculate how many switches are flipped
-    [self.calculator calculateSwitchCountFromSwitchValue:childSwitchValue];
-    
-    // set the value of the master switch as needed
-    [self setMasterSwitchValue];
-}
-
-
-- (IBAction)flippedMasterSwitch:(UISwitch *)masterSwitch
-{
-    
-    NSNumber *masterSwitchValueObject = [NSNumber numberWithBool:self.masterSwitch.on];
-    
-    // send all the child switches to be animated
-    [self.childSwitches makeObjectsPerformSelector:@selector(animateSwitchBasedOnValue:) withObject:masterSwitchValueObject];
-    
-    NSInteger masterSwitchValue = self.masterSwitch.on;
-    
-    // calculate the value of the switch counter based on the master switch
-    [self.calculator resetSwitchCounterToMasterSwitchValue:masterSwitchValue];
-}
-
-
-#pragma mark Helper Calculation Methods
-
-- (void)setMasterSwitchValue
-{
     // if all switches are on, set master switch to on; else set master switch to off
-    if ([self.calculator areAllSwitchesOn]) {
+    if (numberOfSwitchesTurnedOn == totalNumberOfChildSwitches) {
         [self.masterSwitch setOn:YES animated:YES];
     } else {
         [self.masterSwitch setOn:NO animated:YES];
     }
+}
+
+
+- (IBAction)flippedMasterSwitch
+{
+    NSNumber *masterSwitchValueObject = [NSNumber numberWithBool:self.masterSwitch.on];
+    
+    // send all the child switches to be animated
+    [self.childSwitches makeObjectsPerformSelector:@selector(animateSwitchBasedOnValue:) withObject:masterSwitchValueObject];
 }
 
 
